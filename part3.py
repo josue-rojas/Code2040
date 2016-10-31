b9a28218e8ba94e66c1f84e8741f19f5bbf6838e
Josue Rojas <josuerojasrojas@users.noreply.github.com>
2016-10-31 11:15:01 -0400

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,10 +1,26 @@
 import requests
 import re
+import json
 
 
 
 r = requests.post("http://challenge.code2040.org/api/haystack", data={'token':'41d1df9d93182791bd6030e9db134736'})
+""""
+#shorter way
+#this was done after everything was submitted and I found out about json
+#this is not meant to be understood i just wanted to squish everything together
+index =  json.loads(r.text)['haystack'].index(json.loads(r.text)['needle']) #(extra short if you put this straight in the post, but will be too long and confusing
+                             
+r = requests.post("http://challenge.code2040.org/api/haystack/validate", data={'token':'41d1df9d93182791bd6030e9db134736','needle':index})
+
+print r.text
+#if doing it this way ignore everything else
+"""
 text =  r.text #not neccesary but just wanted to see the word
+info = json.loads(r.text)
+print info
+print
+print info['needle']
 print text
 p = re.compile('\w+') #find all words (just the pattern)
 words = p.findall(text)
@@ -23,3 +39,4 @@ print index
 #post it
 r = requests.post("http://challenge.code2040.org/api/haystack/validate", data={'token':'41d1df9d93182791bd6030e9db134736','needle':index})
 print r.text
+
